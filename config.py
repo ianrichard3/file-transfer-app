753d3f6ef05273dff562f8ce1a4286d7f57b5fe3
Ian Richard <90254@sistemas.frc.utn.edu.ar>
2023-07-19 12:11:51 -0300

    avances GUI

---
+++ config.py
@@ -0,0 +1,6 @@
+"""
+ACA tengo que hacer un modulo que maneje toda
+la configuracion del usuario, sea nombre, ip, la theme
+El idioma ponele, etc etc,
+Estaria para crear un archivo de config, averiguar la convencion
+"""
